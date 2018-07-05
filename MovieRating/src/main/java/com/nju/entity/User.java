828bcafe317096e4f6e0d5594b006140fc73da6f
bcy97 <609233559@qq.com>
2018-07-05 21:15:08 +0800

    add 完成登录

--- MovieRating/src/main/java/com/nju/entity/User.java
+++ MovieRating/src/main/java/com/nju/entity/User.java
@@ -1,7 +1,6 @@
 package com.nju.entity;
 
 import lombok.Data;
-import lombok.Generated;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,7 +17,7 @@ public class User {
 
     @Id
     private int id;
-    private String userId;
+    @Column(name = "userId")
     private String username;//用户名
     private String password;//密码
     private String avatar;//头像url地址
--- MovieRating/src/main/java/com/nju/service/impl/LoginServiceImpl.java
+++ MovieRating/src/main/java/com/nju/service/impl/LoginServiceImpl.java
@@ -1,12 +1,23 @@
 package com.nju.service.impl;
 
+import com.nju.dao.UserDao;
+import com.nju.entity.User;
 import com.nju.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoginServiceImpl implements LoginService{
+public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    UserDao userDao;
+
     @Override
     public boolean login(String username, String password) {
+        User user = userDao.findByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return true;
+        }
         return false;
     }
 }
